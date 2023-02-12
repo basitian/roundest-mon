938a431537b3c5af29233276a53f42ce8f1204f2
Bastian Hunecke <bastian.hunecke@icloud.com>
2023-02-12 22:20:20 +0100

    random pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,42 @@
+import { useState } from 'react';
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
 
 export default function Home() {
-	const hello = trpc.hello.useQuery({ text: 'Basti' });
+	const [ids, setIds] = useState(() => getOptionsForVote());
 
-	if (!hello.data) return <div>Loading...</div>;
+	const [first, second] = ids;
 
-	return <div>{hello.data.greeting}</div>;
+	const firstPokemon = trpc.getPokemonById.useQuery({ id: first });
+	const secondPokemon = trpc.getPokemonById.useQuery({ id: second });
+
+	if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
 	return (
 		<div className='h-screen w-screen flex flex-col justify-center items-center'>
 			<div className=' text-2xl text-center'>Which Pokémon is Rounder?</div>
 			<div className='p-2' />
 			<div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
-				<div className='w-16 h-16 bg-red-200' />
+				<div className='w-64 h-64 flex flex-col'>
+					<img
+						src={firstPokemon.data!.sprites.front_default!}
+						className='w-full'
+					/>
+					<div className='text-center text-xl capitalize mt-[-2rem]'>
+						{firstPokemon.data?.name}
+					</div>
+				</div>
 				<div className='p-8'>Vs</div>
-				<div className='w-16 h-16 bg-red-200' />
+				<div className='w-64 h-64 flex flex-col'>
+					<img
+						src={secondPokemon.data!.sprites.front_default!}
+						className='w-full'
+					/>
+					<div className='text-center text-xl capitalize mt-[-2rem]'>
+						{secondPokemon.data?.name}
+					</div>
+				</div>
+				<div className='p-2' />
 			</div>
 		</div>
 	);
---
+++ src/utils/getRandomPokemon.ts
@@ -0,0 +1,15 @@
+const MAX_DEX_ID = 493;
+
+export function getRandomPokemon(notThisOne?: number): number {
+	const pokedexNumber = Math.floor(Math.random() * MAX_DEX_ID + 1);
+
+	if (pokedexNumber !== notThisOne) return pokedexNumber;
+	return getRandomPokemon(notThisOne);
+}
+
+export function getOptionsForVote() {
+	const firstId = getRandomPokemon();
+	const secondId = getRandomPokemon(firstId);
+
+	return [firstId, secondId];
+}
